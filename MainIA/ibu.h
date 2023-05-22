cfec1f5098d14296bc3e2282fe5ca929c1864417
LENOVO <LENOVO@DESKTOP-O0EE9MU>
2023-05-22 21:00:37 +0700

    menambah 6 mtode

--- MainIA/ibu.h
+++ MainIA/ibu.h
@@ -2,6 +2,17 @@
 #define IBU_H
 
 class ibu {
+public:
+	string nama;
+	vector<anak*> daftar_anak;
 
+	ibu(string pNama) : nama(pNama) {
+		cout << "ibu \"" << nama << "\" ada \n";
+	}
+	~ibu() {
+		cout << "ibu \"" << nama << "\" tidak ada \n";
+	}
+	void tambahAnak(anak*);
+	void cetakAnak();
 };
 #endif // !IBU_H
