6291f6f81037b82ec25dd31565382a5177e9b364
LENOVO <LENOVO@DESKTOP-O0EE9MU>
2023-05-22 20:43:50 +0700

    constructor

--- Asosiasi/Asosiasi.cpp
+++ Asosiasi/Asosiasi.cpp
@@ -33,3 +33,27 @@ public:
 	void tambahPasien(pasien*);
 	void cetakPasien();
 };
+
+void pasien::tambahDokter(dokter* pDokter) {
+	daftar_dokter.push_back(pDokter);
+}
+
+void pasien::cetakdokter() {
+	cout << "Daftar Dokter yang menangani pasien \"" << this->nama << "\":\n";
+	for (auto& a : daftar_dokter) {
+		cout << a->nama << "\n";
+	}
+}
+
+void dokter::tambahPasien(pasien* pPasien) {
+	daftar_pasien.push_back(pPasien);
+	pPasien->tambahDokter(this);
+}
+
+void dokter::cetakPasien() {
+	cout << " Daftar Pasien dari dokter\"" << this->nama << "\":\n";
+	for (auto& a : daftar_pasien) {
+		cout << a->nama << "\n";
+	}
+	cout << endl;
+}
