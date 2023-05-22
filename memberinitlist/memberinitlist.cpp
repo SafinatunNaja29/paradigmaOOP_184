d4f2d6e3cdc34928650fe9f5fc7fad0b804135db
LENOVO <LENOVO@DESKTOP-O0EE9MU>
2023-05-22 20:46:34 +0700

    menambah 5 metode

--- memberinitlist/memberinitlist.cpp
+++ memberinitlist/memberinitlist.cpp
@@ -4,5 +4,16 @@
 using namespace std;
 
 class mahasiswa {
-
+private:
+	const int id;
+	string nama;
+	float nilai;
+public:
+	mahasiswa(int pId, string pNama, float pNilai) : id(pId), nama(pNama), nilai(pNilai) {
+	}
+	~mahasiswa() {
+		cout << "id =" << id << endl;
+		cout << "nama =" << nama << endl;
+		cout << "nilai =" << nilai << endl;
+	}
 };
