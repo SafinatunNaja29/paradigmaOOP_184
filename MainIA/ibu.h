6e1f03dabfadf5df34bd5348b9ba6ea379956c1c
LENOVO <LENOVO@DESKTOP-O0EE9MU>
2023-05-22 20:50:37 +0700

    menambah 6 metode

--- MainIA/ibu.h
+++ MainIA/ibu.h
@@ -3,7 +3,17 @@
 
 
 class ibu {
-
+public:
+	string nama;
+	vector<anak*> daftar_anak;
+	ibu(string pNama) : nama(pNama) {
+		cout << "ibu \"" << nama << "\" ada \n";
+	}
+	~ibu() {
+		cout << "ibu \"" << nama << "\" tidak ada \n";
+	}
+	void tambahAnak(anak*);
+	void cetakAnak();
 };
 #endif // IBU_H
 
