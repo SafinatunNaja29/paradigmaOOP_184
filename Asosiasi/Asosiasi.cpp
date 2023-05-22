6b6e97af6d805689578a98865f393f4221f2d7db
LENOVO <LENOVO@DESKTOP-O0EE9MU>
2023-05-22 20:42:26 +0700

    menambah 6 metode

--- Asosiasi/Asosiasi.cpp
+++ Asosiasi/Asosiasi.cpp
@@ -6,5 +6,15 @@ using namespace std;
 class dokter; 
 
 class pasien {
-
+public:
+	string nama;
+	vector<dokter*> daftar_dokter;
+	pasien(string pNama) : nama(pNama) {
+		cout << " Pasien \"" << nama << "\" ada\n";
+	}
+	~pasien() {
+		cout << "pasien \"" << nama << "\" tidak \n";
+	}
+	void tambahDokter(dokter*);
+	void cetakdokter();
 };
