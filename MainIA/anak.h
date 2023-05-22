6aa9c0ea7736e80f1f0034baadc1f6517efd3dcc
LENOVO <LENOVO@DESKTOP-O0EE9MU>
2023-05-22 20:52:53 +0700

    menambah 3 metode

--- MainIA/anak.h
+++ MainIA/anak.h
@@ -2,7 +2,14 @@
 #define ANAK_H
 
 class anak {
-
+public:
+	string nama;
+	anak(string pNama) : nama(pNama) {
+		cout << " anak \"" << nama << "\" ada \n";
+	}
+	~anak() {
+		cout << "anak \"" << nama << "\" tidak ada \n";
+	}
 };
 
 #endif // !1
